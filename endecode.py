03d7f69b22c74774d00af461f478be2ea60d3859
Thies Mueller <github@td00.de>
2022-10-28 22:25:51 +0000

    added the "closed" algorithm for transparency

--- endecode.py
+++ endecode.py
@@ -1,12 +1,27 @@
 #!/bin/env python3
 
-# IMPORTANT! THIS IS ONLY AN EXAMPLE SCRIPT!
-# DO NOT USE THIS DIRECTLY FOR MORE EFFICENT PRIVACY!
-# BECAUSE THIS CODE IS VERY EASY TO BREAK AND OPENSOURCE!
-# YOU CAN RE-WRITE THE FUNCTIONS WITHIN SAME FILENAME.
-# USE THE SAME FUNCTION NAMES.
+from random import choice, shuffle
 
-def en(code:str):
-	return f"salt{code[::-1]}salt"
-def de(code:str):
-    return code[4:-4][::-1]
+def en(code = None):
+
+    def serpirator(inp):
+        letters = 'QWERTYUIOPLKJHGFDSZXCVBNMwertyiolhgfdsazxcvbnmjpk:;()|_-&@.,?! =[(#)]'
+        out = ''
+        for i in inp:
+            s = [
+                i,
+                choice(letters)]
+            shuffle(s)
+            out += ''.join(s)
+        return out
+
+    result = ''
+    for i in code:
+        result += str(ord(i)) + choice('Aqu')
+    return serpirator(result)
+
+
+def de(code):
+    for i in 'QWERTYUIOPLKJHGFDSZXCVBNMwertyiolhgfdsazxcvbnmjpk:;()|_-&@.,?! =[(#)]':
+        code = code.replace(i, '')
+    return ''.join((lambda .0: [ chr(int(i)) for i in .0 ])((lambda .0: [ k for i in .0 for j in i.split('q') for k in j.split('u') ])(code.split('A'))[:-1]))
